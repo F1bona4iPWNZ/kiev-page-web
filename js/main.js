a815a4345b6e8eb8a20c90de8c1009ed6c8cfe73
Oleksandr Hontar <60856881+F1bona4iPWNZ@users.noreply.github.com>
2026-03-24 22:53:55 +0200

    footer

--- js/main.js
+++ js/main.js
@@ -1,52 +1,46 @@
 var swiper = new Swiper(".benefits__container", {
-
   slidesPerView: 1,
-  // spaceBetween: 10,
-  loop:true,
+  loop: true,
   speed: 1600,
   autoplay: {
     delay: 3000,
   },
 
-  // breakpoints: {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  //   // 320: {
-  //   //   slidesPerView: 2,
-  //   //   spaceBetween: 20
-  //   // },
-
-  //   480: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30
-  //   },
-  
-  //   640: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40
-  //   }
-  // },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-
-    const NewSwiper = new SwiperTours('.tours__slider', {
-  slidesPerView: 'auto',
+var swiperTour = new Swiper(".tours__container", {
+  slidesPerView: "auto",
   spaceBetween: 30,
-  centeredSlides: true,
+  centeredSlides: false,
 
   loop: true,
 
   grabCursor: true,
+});
 
-  breakpoints: {
-    768: {
-      centeredSlides: false,
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
+// rating
+document.querySelectorAll(".tours__ratio").forEach((rateBlock) => {
+  const reviewId = rateBlock.dataset.reviewId;
+  const storageKey = `tours-rating-${reviewId}`;
+
+  const savedRating = localStorage.getItem(storageKey);
+
+  if (savedRating) {
+    const savedInput = rateBlock.querySelector(
+      `.rating__input[value="${savedRating}"]`,
+    );
+    if (savedInput) {
+      savedInput.checked = true;
     }
   }
+
+  rateBlock.querySelectorAll(".rating__input").forEach((input) => {
+    input.addEventListener("change", () => {
+      localStorage.setItem(storageKey, input.value);
+    });
+  });
 });
